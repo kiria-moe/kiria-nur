af60c3ee6863e1e7b614d72459688d088bf7e003
kiria-moe <aegisa7280@gmail.com>
2025-05-09 21:19:18 +0800

    feat: add module suurgio-gateway

---
+++ modules/surgio-gateway/default.nix
@@ -0,0 +1,54 @@
+{ config, pkgs, lib, ... }:
+
+{
+  options = {
+    services.surgio-gateway = {
+      enable = lib.mkEnableOption "surgio-gateway";
+      src = lib.mkOption {
+        type = lib.types.path;
+        description = "Source used by surgio-gateway";
+      };
+      address = lib.mkOption {
+        type = lib.types.str;
+        default = "0.0.0.0";
+        description = "Address to bind";
+      };
+      port = lib.mkOption {
+        type = lib.types.number;
+        default = 3000;
+        description = "Port to listen on";
+      };
+      openFirewall = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = "Whether to open the specified ports in the firewall.";
+      };
+    };
+  };
+
+  config = let
+    cfg = config.services.surgio-gateway;
+  in lib.mkIf cfg.enable {
+    systemd.services."surgio-gateway" = {
+      enable = true;
+      unitConfig = {
+        Description = "Generating rules for Surge, Clash, Quantumult like a PRO";
+        Documentation = "https://surgio.js.org/";
+        After = [ "network.target" "nss-lookup.target" ];
+      };
+      serviceConfig = {
+        DynamicUser = "yes";
+        CapabilityBoundingSet = [ "CAP_NET_BIND_SERVICE" ];
+        AmbientCapabilities = [ "CAP_NET_BIND_SERVICE" ];
+        NoNewPrivileges = true;
+        ExecStart = "${pkgs.callPackage ../../pkgs/surgio-gateway { inherit (cfg) src; }}/bin/surgio-gateway ${cfg.address} ${cfg.port}";
+        Restart = "on-failure";
+        RestartSec = "10s";
+        LimitNOFILE = "infinity";
+      };
+      wantedBy = [ "multi-user.target" ];
+    };
+
+    networking.firewall.allowedTCPPorts = lib.optionals cfg.openFirewall [ cfg.port ];
+  };
+}
