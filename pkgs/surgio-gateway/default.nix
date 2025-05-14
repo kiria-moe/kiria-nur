8cad6c0238ec89967bfc7a02d9ed423360f85616
kiria-moe <aegisa7280@gmail.com>
2025-05-14 21:02:11 +0800

    fix(pkgs/surgio-gateway): cd to src before run

--- pkgs/surgio-gateway/default.nix
+++ pkgs/surgio-gateway/default.nix
@@ -18,7 +18,8 @@ buildNpmPackage {
   };
 
   postInstall = ''
-    ${gnused}/bin/sed -i "2s#^#NODE_PATH=$out/lib/node_modules/surgio-gateway/node_modules/ &#" $out/bin/surgio-gateway
+    ${gnused}/bin/sed -i "2icd \$SURGIO_PROJECT_DIR" $out/bin/surgio-gateway
+    ${gnused}/bin/sed -i "3s#^#NODE_PATH=$out/lib/node_modules/surgio-gateway/node_modules/ &#" $out/bin/surgio-gateway
   '';
 
   npmDepsHash = "sha256-vnX9+Td8g2IWnpqvxZA+uYA6T8QU3ExTU7eA9bm1GYo=";
